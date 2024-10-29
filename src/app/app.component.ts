6010825941e5238c3e663af42f45e4e6fc8d75b7
pabvlov <pablojavierprietocepeda@gmail.com>
2024-10-29 12:50:57 -0300

    header y prods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    MatCardModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'reiblanc';
+
+export class AppComponent implements OnInit{
+  screenWidth!: number;
+  @HostListener('window:resize', ['$event'])
+
+  onResize(event: Event) {
+    this.screenWidth = window.innerWidth;
+  }
+
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth
+  }
 }
